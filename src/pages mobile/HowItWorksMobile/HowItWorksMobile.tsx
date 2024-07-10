9741f527ff022757936f5eceffda583409f79bc2
Koren <korenkpalan96@gmail.com>
2024-07-10 16:23:12 +0300

    Upgraded How it works mobile

--- src/pages mobile/HowItWorksMobile/HowItWorksMobile.tsx
+++ src/pages mobile/HowItWorksMobile/HowItWorksMobile.tsx
@@ -5,6 +5,7 @@ import {backgroundColor} from '../../utils/variables'
 import ThreeStepsMobile from './ThreeStepsMobile';
 import FutureUpgradesMobile from './FutureUpgradesMobile';
 import Line from '../../components/Line/Line';
+import jobDescriptionhighlited from '../../assets/images/Job description highlited.png'
 const HowItWorksMobile: React.FC = () => {
   const headerStyle:CSSProperties = {
     fontSize:'35px',
@@ -20,6 +21,76 @@ const HowItWorksMobile: React.FC = () => {
         </div>
               <ThreeStepsMobile/>
         </>
+        <div className="imagesDivMobile">
+          <div className="">
+            <h2 className='resultDivHeaderMobile'>This is How our system looks at Job Descriptions</h2>
+          <img src={jobDescriptionhighlited} alt="" />
+          </div>
+
+          {/* <DescriptionResultDivDesktop/> */}
+          <div className="descriptionResultDivMobile">
+            <h2 className='resultDivHeaderMobile'>After Text Analysis & Data Processing</h2>
+            <div className="resultListsDivMobile">
+            <p style={{textAlign:'center'}}><strong >Role: </strong> Backend Developer</p>
+            <div className="rowMobile">
+              <div className="col1Mobile">
+              <strong>Programming Languages:</strong>
+            <ul>
+              <li>Typescript</li>
+              <li>Java</li>
+              <li>Python</li>
+              <li>Javascript</li>
+              <li>Scala</li>
+            </ul>
+              </div>
+              <div className="col2Mobile">
+              <strong>Databases:</strong>
+            <ul>
+              <li>MongoDB</li>
+              <li>Redis</li>
+              <li>PostgreSQL</li>
+
+            </ul>
+              </div>
+            </div>
+            <div className="rowMobile">
+              <div className="col1Mobile">
+              <strong>Cloud Architecture:</strong>
+            <ul>
+              <li>Microservices</li>
+            </ul>
+              </div>
+              <div className="col2Mobile">
+              <strong>Top Libraries:</strong>
+            <ul>
+              <li>Express.js</li>
+            </ul>
+              </div>
+            </div>
+
+            <div className="rowMobile">
+              <div className="col1Mobile">
+              <strong>Methodologies:</strong>
+            <ul>
+              <li>Agile</li>
+            </ul>
+              </div>
+              <div className="col2Mobile">
+              <strong>Frameworks:</strong>
+            <ul>
+              <li>Nest.js</li>
+              <li>Node.js</li>
+
+            </ul>
+
+              </div>
+            </div>
+
+
+            </div>
+
+          </div>
+        </div>
             <Line height='1px' width='80%' color='white' />
         <>
         <div style={{textAlign:'center'}}>
