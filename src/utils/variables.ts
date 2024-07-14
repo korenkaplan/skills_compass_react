09ec9884037182101c817ff274665d339e6ccb8a
Koren <korenkpalan96@gmail.com>
2024-07-14 16:48:03 +0300

    set debug animation to false

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -4,6 +4,6 @@ const backgroundColor: string = '#253439';
 const sideMenuBackgroundColor: string = '#1E2028';
 const resumeDownloadLink: string = "https://drive.google.com/uc?export=download&id=10Tpl27oSImgvjaUK7aRlspeGQWAfKCqA"
 const textColor: string = '#F4F2EF';
-const framerMotionRepeatOnce: boolean = false;
+const framerMotionRepeatOnce: boolean = true;
 
 export { backgroundColor, textColor, resumeDownloadLink, sideMenuBackgroundColor, framerMotionRepeatOnce};
