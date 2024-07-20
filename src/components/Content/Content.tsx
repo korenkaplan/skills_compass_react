f2fa21cf8e68e3da2d0a6123f6bf237e264a9cc4
Koren <korenkpalan96@gmail.com>
2024-07-20 14:34:15 +0300

    Fix app BAr

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Section } from '../../utils/interfaces';
 import { DrawerDesktopProps } from '../../components/Drawer/Drawer'; // Adjust the import according to your actual Drawer component's props location
 import { DrawerMobileProps } from '../../components/DrawerMobile/DrawerMobile';
-import { appBarHeight } from '../../utils/variables';
 
 interface ContentProps {
   isMobile: boolean;
